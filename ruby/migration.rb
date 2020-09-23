9156b8320df0d7b66e1e7c059062f387382aae3d
Samuel José Gutiérrez Avilés <samaviles@gmail.com>
2020-09-22 19:08:29 -0600

    para local

--- ruby/migration.rb
+++ ruby/migration.rb
@@ -20,14 +20,14 @@ require_relative '.credentials'
 def getQuery(queryIndex)
   {
     0 => 'SELECT *, NOW() AS created_at, NOW() AS updated_at FROM countries ORDER BY created_at ASC;',
-    1 => 'SELECT users.id, users.auth_token, people.birth_date as birthday, users.current_sign_in_at, users.current_sign_in_ip, users.email, users.encrypted_password, users.last_sign_in_at, users.last_sign_in_ip, users.passwd_login, people.phone_number AS phone, users.remember_created_at, users.reset_password_token, users.reset_password_sent_at, CASE WHEN users.role = "admin" THEN 1 WHEN users.role = "general" THEN 2 WHEN users.role = "doctor" THEN 3 WHEN users.role = "employee" THEN 4 WHEN users.role = "evaluator" THEN 5 ELSE NULL END AS role, users.sign_in_count, CASE WHEN people.neighbourhood_id > 0 THEN 3166 ELSE 3166 END AS country_id, users.created_at, users.updated_at FROM users, people WHERE people.user_id = users.id ORDER BY users.created_at ASC;',
+    1 => 'SELECT users.id, users.auth_token, people.birth_date as birthday, users.current_sign_in_at, users.current_sign_in_ip, "", users.email, users.encrypted_password, users.last_sign_in_at, users.last_sign_in_ip, users.passwd_login, people.phone_number AS phone, users.remember_created_at,  users.reset_password_sent_at, users.reset_password_token, CASE WHEN users.role = "admin" THEN 1 WHEN users.role = "general" THEN 2 WHEN users.role = "doctor" THEN 3 WHEN users.role = "employee" THEN 4 WHEN users.role = "evaluator" THEN 5 ELSE NULL END AS role, users.sign_in_count, CASE WHEN people.neighbourhood_id > 0 THEN 3166 ELSE 3166 END AS country_id, users.created_at, users.updated_at FROM users, people WHERE people.user_id = users.id ORDER BY users.created_at ASC;',
     2 => 'SELECT people.id, people.last_name, people.name, people.is_male AS sex, users.stripe_id, users.stripe_subscription_id, people.user_id, people.created_at, people.updated_at FROM people, users WHERE people.user_id = users.id AND people.user_id IS NOT NULL ORDER BY people.created_at ASC;',
     3 => 'SELECT id, available, avatar, header, last_seen_at, minsa_number, self_description, signature, CASE WHEN specialty = "General" THEN 1 WHEN specialty = "Medico General" THEN 1 WHEN specialty = "Nutricionista" THEN 2 ELSE NULL END AS specialty, years_of_experience, user_id, created_at, updated_at FROM doctors WHERE user_id IS NOT NULL ORDER BY created_at ASC;',
     4 => 'SELECT telemedicines.id, telemedicines.hear_about_us, telemedicines.our_services_result, telemedicines.profesional_actions_result, telemedicines.profesional_conection_result, telemedicines.profesional_interaction_result, telemedicines.satisfaction_result, people.id AS person_id, telemedicines.created_at, telemedicines.updated_at FROM telemedicines, people, users WHERE telemedicines.user_id = users.id AND people.user_id = users.id ORDER BY telemedicines.created_at ASC;',
     5 => 'SELECT sessions.id, 0, sessions.person_id, sessions.created_at, sessions.updated_at FROM sessions WHERE sessions.person_id IN (SELECT id FROM people WHERE user_id IS NOT NULL) ORDER BY sessions.created_at ASC;',
     6 => 'SELECT credit_users.id, credit_users.credit, people.id AS person_id, credit_users.created_at, credit_users.updated_at FROM credit_users, people, users WHERE credit_users.user_id = users.id AND people.user_id = users.id ORDER BY people.created_at ASC;',
-    7 => 'SELECT id, finished_at, finished, CASE specialty = "Medico General" THEN 1 WHEN specialty = "Nutricionista" THEN 2 WHEN specialty = "NUTRIMIND_TEST_PLAN" THEN 4 ELSE NULL END AS specialty, taken, CASE WHEN service_type = "paid" THEN 1 WHEN service_type = "free" THEN 2 ELSE 0 END AS service, twilio_channel_name, twilio_channel_sid, CASE WHEN examination_type = "chat" THEN 1 WHEN examination_type = "video" THEN 2 WHEN examination_type = "expediente" THEN 3 WHEN examination_type = "NUTRIMIND_TEST_PLAN" THEN 4 ELSE NULL END AS type, doctor_id, person_id, created_at, updated_at FROM examinations WHERE doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL) ORDER BY created_at ASC;',
-    8 => 'SELECT id, DATE(appointment_date) AS date, TIME(appointment_date) AS hour, reason, CASE specialty = "Medico General" THEN 1 WHEN specialty = "Nutricionista" THEN 2 WHEN specialty = "Psicología" THEN 3 WHEN specialty = "Plan nutricional" THEN 4 ELSE NULL END AS specialty, CASE WHEN status = "scheduled" THEN 1 WHEN status = "pending" THEN 2 WHEN status = "finished" THEN 3 ELSE NULL END AS status, taken, CASE WHEN ap_type = "plan" THEN 1 ELSE NULL END AS type, doctor_id, person_id, created_at, updated_at FROM appointments WHERE person_id IS NOT NULL AND doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL) ORDER BY created_at ASC;',
+    7 => 'SELECT id, CASE WHEN examination_type = "chat" THEN 1 WHEN examination_type = "video" THEN 2 WHEN examination_type = "expediente" THEN 3 WHEN examination_type = "NUTRIMIND_TEST_PLAN" THEN 4 ELSE NULL END AS examination_type, finished_at, finished, CASE WHEN service_type = "paid" THEN 1 WHEN service_type = "free" THEN 2 ELSE 0 END AS service, CASE WHEN specialty = "Medico General" THEN 1 WHEN specialty = "Nutricionista" THEN 2 WHEN specialty = "NUTRIMIND_TEST_PLAN" THEN 4 ELSE NULL END AS specialty, taken, twilio_channel_name, twilio_channel_sid, doctor_id, person_id, created_at, updated_at FROM examinations WHERE doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL) ORDER BY created_at ASC;',
+    8 => 'SELECT id, DATE(appointment_date) AS date, TIME(appointment_date) AS hour, reason, CASE WHEN specialty = "Medico General" THEN 1 WHEN specialty = "Nutricionista" THEN 2 WHEN specialty = "Psicología" THEN 3 WHEN specialty = "Plan nutricional" THEN 4 ELSE NULL END AS specialty, CASE WHEN status = "scheduled" THEN 1 WHEN status = "pending" THEN 2 WHEN status = "finished" THEN 3 ELSE NULL END AS status, taken, CASE WHEN ap_type = "plan" THEN 1 ELSE NULL END AS type, doctor_id, person_id, created_at, updated_at FROM appointments WHERE person_id IS NOT NULL AND doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL) ORDER BY created_at ASC;',
     9 => 'SELECT id, end_date, start_date, status, person_id, created_at, updated_at FROM memberships ORDER BY created_at ASC;',
     10 => 'SELECT CAST(@rownum := @rownum + 1 AS UNSIGNED) AS id, p.diag_diabetes AS diabetes, p.exercise, p.health_condition AS health, p.healthy_foods, p.diag_heart AS heart, p.highpressure, p.mental_health_condition AS mental_health, p.diag_obesity AS obesity, p.does_smoke AS smoke, p.id AS person_id, p.created_at, p.updated_at FROM people p, (SELECT @rownum := 0) t WHERE p.user_id IS NOT NULL ORDER BY created_at ASC;',
     11 => 'SELECT waist_widths.id, CASE WHEN body_mass_indices.cintura_cat = "Normal" THEN 1 WHEN body_mass_indices.cintura_cat = "Riesgo aumentado" THEN 2 WHEN body_mass_indices.cintura_cat = "Riesgo muy aumentado" THEN 3 ELSE NULL END AS category, waist_widths.risk_category AS risk, waist_widths.weight AS waist, waist_widths.session_id, waist_widths.created_at, waist_widths.updated_at FROM waist_widths, body_mass_indices WHERE body_mass_indices.session_id = waist_widths.session_id ORDER BY waist_widths.created_at ASC;',
@@ -36,7 +36,8 @@ def getQuery(queryIndex)
     14 => 'SELECT id, CAST(health AS INT) AS health, risk_category AS risk, session_id, created_at, updated_at FROM health_states ORDER BY created_at ASC;',
     15 => 'SELECT id, comments, friday_breakfast, friday_dinner, friday_lunch, friday_snack_dinner, friday_snack_lunch, friday_snack, monday_breakfast, monday_dinner, monday_lunch, monday_snack_dinner, monday_snack_lunch, monday_snack, saturday_breakfast, saturday_dinner, saturday_lunch, saturday_snack_dinner, saturday_snack_lunch, saturday_snack, sunday_breakfast, sunday_dinner, sunday_lunch, sunday_snack_dinner, sunday_snack_lunch, sunday_snack, thursday_breakfast, thursday_dinner, thursday_lunch, thursday_snack_dinner, thursday_snack_lunch, thursday_snack, tuesday_breakfast, tuesday_dinner, tuesday_lunch, tuesday_snack_dinner, tuesday_snack_lunch, tuesday_snack, wednesday_breakfast, wednesday_dinner, wednesday_lunch, wednesday_snack_dinner, wednesday_snack_lunch, wednesday_snack, examination_id, created_at, updated_at FROM nutritional_plans WHERE examination_id IN (SELECT id FROM examinations WHERE doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL)) ORDER BY created_at ASC;',
     16 => 'SELECT id, plan, reason_attention AS reason, examination_id, created_at, updated_at FROM soaps WHERE examination_id IN (SELECT id FROM examinations WHERE doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL)) ORDER BY created_at ASC;',
-    17 => 'SELECT id, actividad AS activity, anxiety_food, culpability, diag_diabetes AS diabetes, eat_out_frecuency, frecuencia_ejercicio AS exercise_frequency, salud AS health, diag_heart AS heart, altura AS height, imc, animo AS mind, non_pathological_background, situation, cintura AS waist, peso AS weight, examination_id, created_at, updated_at FROM nutrimind_tests WHERE examination_id IN (SELECT id FROM examinations WHERE doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL)) ORDER BY created_at ASC;'
+    17 => 'SELECT id, actividad AS activity, anxiety_food, culpability, diag_diabetes AS diabetes, eat_out_frecuency, frecuencia_ejercicio AS exercise_frequency, salud AS health, diag_heart AS heart, altura AS height, imc, animo AS mind, non_pathological_background, situation, cintura AS waist, peso AS weight, examination_id, created_at, updated_at FROM nutrimind_tests WHERE examination_id IN (SELECT id FROM examinations WHERE doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL)) ORDER BY created_at ASC;',
+    18 => 'SELECT id, activities, examination_id, created_at, updated_at FROM psychological_plans WHERE examination_id IN (SELECT id FROM examinations WHERE doctor_id IS NOT NULL AND doctor_id IN (SELECT id FROM doctors WHERE user_id IS NOT NULL)) ORDER BY created_at ASC;'
   }[queryIndex]
 end
 
@@ -46,6 +47,16 @@ end
 
 def copyToDB(table)
   @pgconn.exec("COPY #{table} FROM '/tmp/#{table}.csv' DELIMITER ',' CSV HEADER;")
+end
+
+def setSessionRisk
+  @pgconn.exec("WITH session_risks AS (SELECT sessions.id AS sessionid, CASE WHEN emotional_states.risk = 3 THEN 3 WHEN health_states.risk = 3 THEN 3 WHEN waist_widths.risk = 3 THEN 3 ELSE body_mass_indices.risk END AS sessionrisk FROM sessions, body_mass_indices, emotional_states, health_states, waist_widths WHERE body_mass_indices.session_id = sessions.id AND emotional_states.session_id = sessions.id AND health_states.session_id = sessions.id AND waist_widths.session_id = sessions.id ) UPDATE sessions SET risk = session_risks.sessionrisk FROM session_risks WHERE sessions.id = session_risks.sessionid;")
+    
+  @pgconn.exec("UPDATE sessions SET risk = body_mass_indices.risk FROM body_mass_indices WHERE sessions.id = body_mass_indices.session_id AND sessions.risk = 0;")
+end
+
+def primaryKeySequence(table)
+  @pgconn.exec("SELECT setval(pg_get_serial_sequence('#{table}', 'id'), MAX(id)) FROM #{table};")
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -67,7 +78,8 @@ if __FILE__ == $PROGRAM_NAME
     14 => 'health_states',
     15 => 'nutritional_plans',
     16 => 'soaps',
-    17 => 'nutrimind_tests'
+    17 => 'nutrimind_tests',
+    18 => 'psychological_plans'
   }
 
   tables.each do |index, table|
@@ -77,11 +89,15 @@ if __FILE__ == $PROGRAM_NAME
       df.write_csv("/tmp/#{table}.csv")
       #puts `cp evital_production/#{table}.csv /tmp/.`
       copyToDB(table)
+      primaryKeySequence(table)
       File.delete("/tmp/#{table}.csv") if File.exist?("/tmp/#{table}.csv")
     rescue Exception => e
       puts e
       File.delete("/tmp/#{table}.csv") if File.exist?("/tmp/#{table}.csv")
       exit
     end
+  end
+  begin
+    setSessionRisk
   end
 end
